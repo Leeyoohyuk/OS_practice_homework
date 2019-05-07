3314fc7a2a1fcdcd864a1fa77ac11ffea3799df7
YooHyuk <azsxd34@naver.com>
2019-05-07 17:30:17 +0900

    OS-lecture HW2 : Finished

--- DMA/DMA/IDEcontroller.cpp
+++ DMA/DMA/IDEcontroller.cpp
@@ -5,6 +5,7 @@
 //#include <condition_variable>
 using namespace std;
 
+// static 초기화
 mutex IDEcontroller::mtx;
 condition_variable IDEcontroller::cv;
 int IDEcontroller::total_preprocess = 0;
@@ -17,7 +18,7 @@ IDEcontroller::IDEcontroller()
 
 void IDEcontroller::setdevice(string device)
 {
-	this->device = device;
+	this->device = device; // 디바이스의 이름을 받아서 IDEcontroller 세팅
 }
 
 void IDEcontroller::assign(int tasksize)
@@ -29,62 +30,62 @@ void IDEcontroller::assign(int tasksize)
 	{
 		if (tasksize == total_preprocess)
 		{ // 모든 작업이 할당됐다면 루프 탈출 및 스레드 종료
-			cout << "\nAll task have been assigned\n\n";
+			cout << "\n All task have been assigned\n\n";
 			break;
 		}
 		if (((double)clock()) / CLOCKS_PER_SEC - start > 0.01)
 		{ // Quantum 0.01이 채워진 경우
-			cout << "\nQuantum out : Context switch\n\n"; // 
+			cout << "\n Quantum out : Context switch\n\n";
 			cv.wait(ul); // 이 스레드를 wait 하고 다른 스레드가 작업 시작
-			cv.notify_one(); // 이 스레드의 작업이 재게되었을 때 wait상태의 다른 스레드를 깨운다
+			cv.notify_one(); // 이 스레드의 작업이 재게되었을 때 wait상태로 들어간 스레드를 깨운다
 			if (tasksize == total_preprocess) // 다른 스레드에서 task assign이 모두 완료된 경우 루프 탈출 및 스레드 종료
 			{
 				break;
 			}
 			start = ((double)clock()) / CLOCKS_PER_SEC; // 스레드가 다시 작업을 재게한 시각을 기록한다 (Quantum 시작)
 		}
-		cout << "PID : <" << device << 'X' << ++length << "> is assigned" << endl;
+		cout << " PID : <" << device << 'X' << ++length << "> is assigned" << endl;
 		controller.push(length); // IDEcontroller의 큐에 작업할당
-		++total_preprocess; // preprocess 증가
+		++total_preprocess; // 총 할당 프로세스 증가
 	}
 }
 
 void IDEcontroller::processing(queue<pair<string,int>> *DMAcon, int tasksize)
 {
-	unique_lock<mutex> ul(mtx);
-	cv.notify_one();
+	unique_lock<mutex> ul(mtx); // mutex 보장
+	cv.notify_one(); // 임계영역에 들어오면 wait 상태인 스레드를 깨운다 -> 이 스레드가 wait가 될 경우 다른 스레드가 작업 진행할 수 있도록
 	while (1)
 	{
-		if (controller.size() == 0)
+		if (controller.size() == 0) // 컨트롤러에 할당된 작업이 없다면
 		{
-			cout << "\nThere isn't task associated with " << device << endl << endl;
+			cout << "\n There isn't task associated with " << device << endl << endl;
 			while (controller.size() == 0)
 			{
-				if (tasksize == total_postprocess)
+				if (tasksize == total_postprocess) // 컨트롤러에 작업이 없고 모든 프로세스의 처리가 완료되었다면
 				{
-					break;
+					break; // 루프를 탈출
 				}
-				cv.wait(ul);
-				cv.notify_all();
-				if (tasksize == total_postprocess)
+				cv.wait(ul); // 그렇지 않으면 컨트롤러에 process가 새로 assign될 수 있기때문에 wait상태로 들어간다
+				cv.notify_all(); // 작업 재게시 wait 스레드를 깨운다
+				if (tasksize == total_postprocess) // wait에서 깨어났을 때 다른 스레드에서 모든 process가 처리되었다면
 				{
-					break;
+					break; // 루프를 탈출
 				}
 			}
 		}
-		if (tasksize == total_postprocess)
+		if (tasksize == total_postprocess) // 작업이 전부 처리된 경우 이 루프도 탈출시키고 스레드를 종료한다.
 		{
 			break;
 		}
-		cout << "Process : " << device << 'x' << controller.front() << " in progress. \n";
-		total_postprocess++;
+		cout << " Process : " << device << 'x' << controller.front() << " in progress. \n";
+		total_postprocess++; 
 		pair<string, int> temp;
 		temp.first = device;
 		temp.second = controller.front();
-		controller.pop();
-		//여기서 DMA컨트롤러로 processing 된 작업을 pass 
-		DMAcon->push(temp);
-		cv.wait(ul);
-		cv.notify_all();
+		controller.pop(); 
+		DMAcon->push(temp); 
+		// 1. DMA 컨트롤러로 processing 된 작업을 전달 2. 프로세싱된 작업의 수 증가 3. 컨트롤러에서 작업 제거
+		cv.wait(ul); // 한 번에 하나의 process만 처리해야하기 때문에 한 번의 processing 진행되면 다른 스레드가 작업
+		cv.notify_all(); // 작업 재게시 wait 스레드를 깨운다
 	}
 }
--- DMA/DMA/main.cpp
+++ DMA/DMA/main.cpp
@@ -28,8 +28,14 @@ int size()
 int type()
 {
 	int num_of_device = 0;
-	cout << "Please enter the number of type you want to task : ";
+	cout << " Please enter the number of type you want to task (at least 3type) : ";
 	cin >> num_of_device;
+	while (num_of_device < 3) // 3 이하의 type을 입력시 재입력 받도록 함
+	{
+		cout << " Please enter a number of more than three : ";
+		cin >> num_of_device;
+	}
+
 	return num_of_device;
 }
 
@@ -43,7 +49,7 @@ void task_processing()
 
 	IDEcontroller *idecon = new IDEcontroller[num_of_device]; // IDEcontroller 객체 동적할당
 	thread *ide = new thread[num_of_device*2]; // 사용할 devcie 개수 * 2 만큼의 스레드 할당 -> 할당과 처리의 분리
-	queue<pair<string, int>> *DMAcon = new queue<pair<string, int>>;
+	queue<pair<string, int>> *DMAcontroller = new queue<pair<string, int>>;
 	for (int i = 0; i < num_of_device; i++)
 	{
 		int randnum;
@@ -51,27 +57,30 @@ void task_processing()
 			randnum = ranum() % 5;
 		} while (flag[randnum]);
 		flag[randnum] = true;
-
 		idecon[i].setdevice(device[randnum]);
-	} // IDEcontroller에 device type을 저장한다
+	} // IDEcontroller에 랜덤으로 device 설정
 
 	for(int i = 0; i<num_of_device; i++)
 	{
 		ide[i] = thread(&IDEcontroller::assign, &idecon[i], task_size); 
-		ide[i+num_of_device] = thread(&IDEcontroller::processing, &idecon[i], DMAcon, task_size);
+		ide[i+num_of_device] = thread(&IDEcontroller::processing, &idecon[i], DMAcontroller, task_size);
 	} // 멀티스레딩을 이용하여 assign - processing 작업 시작
 	for (int i = 0; i < num_of_device * 2; i++)
 	{
 		ide[i].join();
 	} // 모든 작업 종료 시 알림
-	cout << "All task on IDEcontroller have been processed\n";
+	cout << " All task on IDEcontroller have been processed\n";
 	cout << "\n Start assigning on the memory buffer\n\n";
-	while (DMAcon->size())
+	Sleep(5000); // 출력확인 용 sleep
+
+	while (DMAcontroller->size())
 	{
-		cout << DMAcon->front().first << 'X' << DMAcon->front().second << " have been assigned on the memory buffer\n";
-		DMAcon->pop();
+		cout << ' ' << DMAcontroller->front().first << 'X' << DMAcontroller->front().second << " have been transfered on the memory buffer\n";
+		DMAcontroller->pop(); // 메모리 버퍼에 전달
 	}
 	cout << endl;
+	cout << " DMAcontroller finished task transmission to memorybuffer";
+	// DMAcontroller 작업 종료 시 알림
 
 	return;
 }
